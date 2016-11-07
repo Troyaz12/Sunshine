cd70f918b7cb21eaba7acd39844ff147de4085ee
Troy Gugler <guglert80@gmail.com>
2016-11-06 20:27:19 -0700

    dataitems connected to watch added dataitems to main activity and created wear service to connect to the watchface. Created new watchface in same package as the app. deleted the old one.

---
+++ watchface/src/main/java/com/example/android/sunshine/app/MyService.java
@@ -0,0 +1,46 @@
+package com.example.android.sunshine.app;
+
+import android.content.Intent;
+import android.support.v4.content.LocalBroadcastManager;
+
+import com.google.android.gms.wearable.DataEvent;
+import com.google.android.gms.wearable.DataEventBuffer;
+import com.google.android.gms.wearable.DataMap;
+import com.google.android.gms.wearable.DataMapItem;
+import com.google.android.gms.wearable.WearableListenerService;
+
+public class MyService extends WearableListenerService {
+    public MyService() {
+    }
+
+    Double high;
+    Double low;
+    @Override
+    public void onDataChanged(DataEventBuffer dataEventBuffer) {
+        super.onDataChanged(dataEventBuffer);
+
+        for(DataEvent dataEvent:dataEventBuffer){
+            if(dataEvent.getType()==DataEvent.TYPE_CHANGED){
+                DataMap dataMap = DataMapItem.fromDataItem(dataEvent.getDataItem()).getDataMap();
+                String path = dataEvent.getDataItem().getUri().getPath();
+                if (path.equals("/message")){
+                    high = dataMap.getDouble("high");
+                    low = dataMap.getDouble("low");
+
+                    System.out.println("Message wear is: "+high);
+                }
+
+            }
+        }
+        // Broadcast message to wearable activity for display
+        Intent messageIntent = new Intent();
+        messageIntent.setAction(Intent.ACTION_SEND);
+        messageIntent.putExtra("high", high.toString());
+        messageIntent.putExtra("low", low).toString();
+
+        LocalBroadcastManager.getInstance(this).sendBroadcast(messageIntent);
+        System.out.println("Message wear is: "+high);
+
+
+    }
+}
